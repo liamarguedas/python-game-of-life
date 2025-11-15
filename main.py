8a58efc9c338c31cdf10d853b6a8ad82a7cf81c7
liano <iliamftw2013@gmail.com>
2025-11-15 11:06:40 -0300

    add: pygame and python structure

--- main.py
+++ main.py
@@ -1,5 +1,23 @@
+from source import Game
+
+
+BACKGROUND_COLOR = "black"
+CELL_COLOR = "white"
+CELL_SIZE = 10
+
+
 def main():
-    print("Hello from python-game-of-life!")
+
+    g = Game()
+    screen = g.get_screen()
+
+    g.create_canvas()
+
+    while True:
+
+        screen.fill(BACKGROUND_COLOR)
+
+        g.render_canvas()
 
 
 if __name__ == "__main__":
---
+++ source/game.py
@@ -0,0 +1,22 @@
+import pygame
+
+
+class Game:
+
+    def __init__(self, FPS: int = 30, WIDTH: int = 1000, HEIGHT: int = 1000):
+
+        self.FPS = FPS
+        self.WIDTH = WIDTH
+        self.HEIGHT = HEIGHT
+        self.game_instance = pygame
+
+    def create_canvas(self):
+        self.game_instance.init()
+
+    def render_canvas(self):
+        self.game_instance.display.flip()
+        self.game_instance.time.Clock().tick(self.FPS)
+
+    def get_screen(self):
+        screen = self.game_instance.display.set_mode((self.WIDTH, self.HEIGHT))
+        return screen
